d5e2e47c2c80a07bfbf32461102e3e61408cda5f
Ramkumar R <ramkumar4271@gmail.com>
2022-02-02 16:59:38 +0530

    Color Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'color-assignment';
+export class AppComponent implements OnInit{
+
+  myControl = new FormControl();
+  options: any[] = [
+    {"name":"Gray", "code":"#495057"},
+    {"name":"Grape", "code":"#ae3ec9"},
+    {"name":"Blue", "code":"#1c7ed6"},
+    {"name":"Green", "code":"#37b24d"},
+    {"name":"Orange", "code":"#f76707"},
+    {"name":"Red", "code":"#f03e3e"},
+    {"name":"Violet", "code":"#7048e8"},
+    {"name":"Cyan", "code":"#1098ad"},
+    {"name":"Lime", "code":"#74b816"},
+    {"name":"Pink", "code":"#d6336c"},
+    {"name":"Indigo", "code":"#4263eb"},
+    {"name":"Teal", "code":"#0ca678"},
+    {"name":"Yellow", "code":"#f59f00"}
+  ];
+  filteredOptions: Observable<any[]> | undefined;
+  selectedColors: any[] = [];
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(name: string): any[] {
+    const filterValue = name.toLowerCase();
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  onSelect(color: any) {
+    this.selectedColors.push(color);
+  }
+
+  onRemove(color: any) {
+    const index = this.selectedColors.findIndex(obj => obj.code === color.code);
+    this.selectedColors.splice(index,1)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
